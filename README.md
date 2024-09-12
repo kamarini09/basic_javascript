842db258d0a5e3e31e1ff0779be9d2578c5d78ad
kamarini <>
2024-09-12 23:16:38 +0200

    end for today

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Hello!
 Here someone can find the assignments of the 2 first weeks.
 They are 6 folders:
-the first day assignments : js_basics
-the second day assignments : DOM_Events + rubber_duck
-the third day assignments : loops and web2024ifelseloop
+First day assignments : js_basics
+Second day assignments : DOM_Events + rubber_duck
+Third day assignments : loops and web2024ifelseloop
+Fourth day : rubber_duck
--- rubber_duck/duck.js
+++ rubber_duck/duck.js
@@ -1,5 +1,6 @@
 const duck = document.querySelector(".duck");
 
+//logs for hovering
 duck.addEventListener("mouseover", function () {
   console.log(
     "Wuuuuuuu! You finally met the lille easter duck egg! He was born to code!"
@@ -9,11 +10,42 @@ duck.addEventListener("mouseover", function () {
 duck.addEventListener("mouseout", function () {
   console.log("Seems like you didn't like it :((((");
 });
+//------------------------------------------------------------------
+function saveMessages(messages) {
+  localStorage.setItem("messages", JSON.stringify(messages));
+}
+
+function loadMessages() {
+  const messages = JSON.parse(localStorage.getItem("messages")) || [];
+  return messages;
+}
+
+function displayMessages() {
+  const messageList = document.getElementById("messageList");
+  messageList.innerHTML = "";
+
+  const messages = loadMessages();
+
+  messages.forEach((message) => {
+    const li = document.createElement("li");
+    li.textContent = message;
+    messageList.appendChild(li);
+  });
+}
+
+window.addEventListener("load", displayMessages);
 
 const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", function () {
   const problemInput = document.getElementById("problemInput").value;
 
+  if (problemInput) {
+    const messages = loadMessages();
+    messages.push(problemInput);
+    saveMessages(messages);
+    displayMessages();
+    document.getElementById("problemInput").value = ""; //cleans the input field
+  }
   console.log(problemInput);
 });
