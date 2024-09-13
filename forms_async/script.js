67025437cb7590203902b4256bc295f91f549d31
kamarini <>
2024-09-13 09:27:29 +0200

    still an error with await

--- forms_async/script.js
+++ forms_async/script.js
@@ -1,10 +1,13 @@
+const name = prompt("What is your name?");
+console.log(name);
+
 async function fetchMoviesJSON() {
   const response = await fetch("https://v2.jokeapi.dev/joke/Any");
   const jokeObject = await response.json();
   return jokeObject;
 }
-// fetchMoviesJSON().then(movies => {
-//     console.log(movies); // fetched movies
-// });
-const jokeObject = await fetchMoviesJSON();
-console.log(jokeObject.joke);
+fetchMoviesJSON().then((movies) => {
+  console.log(movies); // fetched movies
+});
+// const jokeObject = await fetchMoviesJSON();
+// console.log(jokeObject.joke);
